521aa0020bf05324920900cdb9b7b209187a53f4
wbbwbwbwbwbb <chemo-antsy-series@duck.com>
2026-04-13 17:44:45 -0700

    Update install-deps.sh

--- sdata/dist-arch/install-deps.sh
+++ sdata/dist-arch/install-deps.sh
@@ -17,6 +17,7 @@ remove_deprecated_dependencies(){
   local list=()
   list+=(illogical-impulse-{microtex,pymyc-aur,oneui4-icons-git})
   list+=(hyprland-qtutils)
+  list+=(quickshell-git)
   list+=(matugen-bin)
   for i in ${list[@]};do try sudo pacman --noconfirm -Rdd $i;done
 }
